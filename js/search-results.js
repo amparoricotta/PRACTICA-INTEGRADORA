887165a794f873ef71330213985f8fdbd59cd0f5
tlourdes <tlourdes@udesa.edu.ar>
2024-11-14 15:23:43 -0300

    agregamos que salte mensaje si lo ingresado en el buscador es menor a 3 caracteres

--- js/search-results.js
+++ js/search-results.js
@@ -4,27 +4,34 @@ let queryString = location.search;
 let queryStringObjeto = new URLSearchParams(queryString);
 let identificador = queryStringObjeto.get('q');
 
-fetch(`https://dummyjson.com/recipes/search?q=${identificador}`)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data);
-        let lista= document.querySelector('.seccionSR')
-        let buscar=""
-        for (i=0; i<data.recipes.length;i++){
-          buscar+=`<article>
-        <img src=  ${data.recipes[i].image} alt=''>
-        <p>${data.recipes[i].name} </p>
-        <p>Nivel de dificultad: ${data.recipes[i].difficulty}</p>
-        <a href="./receta.html">Ir al detalle</a>
-        </article>`
-
+if (identificador && identificador.length >= 3) {
+    fetch(`https://dummyjson.com/recipes/search?q=${identificador}`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+            let lista= document.querySelector('.seccionSR')
+            let buscar=""
+            if (data.recipes.length > 0) {
+                for (let i = 0; i < data.recipes.length; i++) {
+                    buscar += `<article>
+                        <img src="${data.recipes[i].image}" alt="">
+                        <p>${data.recipes[i].name}</p>
+                        <p>Nivel de dificultad: ${data.recipes[i].difficulty}</p>
+                        <a href="./receta.html">Ir al detalle</a>
+                    </article>`;
+                }
+            } else {
+                buscar = "<p>No se encontraron resultados para la búsqueda.</p>";
+            }
+            lista.innerHTML= buscar
+       
+       
+            })
+            .catch(function(error) {
+            console.log("Error: " + error);
+            })
+        }else {
+            resultados.innerHTML = "<p>Por favor, ingresa un término de búsqueda válido de al menos 3 caracteres.</p>";
         }
-        lista.innerHTML= buscar
-       
-       
-       })
-       .catch(function(error) {
-        console.log("Error: " + error);
-       })
