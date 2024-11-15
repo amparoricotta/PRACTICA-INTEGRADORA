582a9524a8bd05fedb550c020bad7cbbcd82efe4
amparoricotta <aricottapensa@udesa.edu.ar>
2024-11-15 11:14:15 -0300

    Html y JS de register y login

--- js/register.js
+++ js/register.js
@@ -1,13 +1,16 @@
-let email =document.querySelector(".emailRegister")
-let boton=document.querySelector(".botonRegister")
-let contraseña =document.querySelector(".contraseñaRegister")
-boton.addEventListener("click", function(){
-    if (email.value=== ""){
+let formulario= document.querySelector("form")
+let email= document.querySelector(".emailRegister")
+let password= document.querySelector(".contraseñaRegister")
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault()
+    if (email.value== ""){
         let mensaje= "Por favor complete el campo email"
         alert(mensaje)
-    }
-    else if (contraseña.value=== ""){
+    }else if (password.value== ""){
         let mensajec="Por favor complete el campo contraseña"
         alert(mensajec)
+    }else{
+        formulario.sumbit()
     }
-})
+    })
+
